916c3ab1209bc90d142dc3edc202ab13d51692ca
Nick Meldrum <nick.meldrum@fil.com>
2019-01-21 13:03:43 +0000

    got sourcemaps working to the js

--- packages/top-level/webpack.config.js
+++ packages/top-level/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
+  devtool: 'eval-source-map',
   entry: './lib/top-level.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
