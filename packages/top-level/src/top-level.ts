8a20ada252eeba010b192f66cfb3033b8e7ef783
Nick Meldrum <nick.meldrum@fil.com>
2019-01-21 11:45:38 +0000

    set up top-level to allow debugging to check for sourcemaps

--- packages/top-level/src/top-level.ts
+++ packages/top-level/src/top-level.ts
@@ -1,3 +1,17 @@
+import { MyModel } from '@nick/shared';
+
 export const handler = () => {
-  return 'hi';
+  const x = new MyModel();
+  debugger;
+  console.log(x.doSomething());
 };
+
+const maxNumberOfIterations = 100;
+let iterationCount = 0;
+
+const timerId = setInterval(() => {
+  if (++iterationCount >= maxNumberOfIterations) {
+    clearInterval(timerId);
+  }
+  handler();
+}, 1000);
